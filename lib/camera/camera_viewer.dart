59c6220d6eb4e81230ce2b0356a558498a1c6775
ralucaib <braturalucaioana@gmail.com>
2024-05-14 13:58:23 +0300

    contur obiecte functioanl

--- lib/camera/camera_viewer.dart
+++ lib/camera/camera_viewer.dart
@@ -1,5 +1,7 @@
+import 'dart:developer';
+
 import 'package:camera/camera.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../scan_controller.dart';
@@ -10,13 +12,38 @@ class CameraViewer extends StatelessWidget {
   @override
     Widget build(BuildContext context) {
       return GetX<ScanController>(builder: (controller) {
+        var isX = controller.x * 100;
+        log("isX = $isX");
         if (!controller.isInitialized) {
           return Container();
         }
         return SizedBox(
             height: Get.height,
             width: Get.width,
-            child: CameraPreview(controller.cameraController));
+            child: Stack(
+              children: [
+                CameraPreview(controller.cameraController),
+                Positioned(
+                  top: controller.y * 700,
+                  right: controller.x * 500,
+                  child: Container(
+                    width: controller.w * 100 * context.width / 100,
+                    height: controller.h * 100 * context.height / 100,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(color: Colors.green, width: 4.0)),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Container(
+                          color: Colors.white,
+                            child: Text(controller.label))
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ));
       });
     }
   }
--- lib/scan_controller.dart
+++ lib/scan_controller.dart
@@ -1,3 +1,7 @@
+
+
+import 'dart:developer';
+
 import 'package:camera/camera.dart';
 import 'package:get/get.dart';
 import 'package:tflite_v2/tflite_v2.dart';
@@ -8,8 +12,21 @@ class ScanController extends GetxController{
   late List<CameraDescription> _cameras;
   int imgCount = 0;
 
+  var x = 0.0;
+  var y = 0.0;
+  var w = 0.0;
+  var h = 0.0;
+  var label = "";
+
   bool get isInitialized => _isInitialized.value;
   CameraController get cameraController => _cameraController;
+
+  @override
+  void onInit() {
+    initCamera();
+    _initTflite();
+    super.onInit();
+  }
 
   @override
   void dispose() {
@@ -20,9 +37,9 @@ class ScanController extends GetxController{
   }
 
   Future<void> _initTflite() async{
-    String? res = await Tflite.loadModel(
-        model: "assets/model.tflite",
-        labels: "assets/etichete.txt",
+    await Tflite.loadModel(
+        model: "assets/ssd_mobilenet.tflite",
+        labels: "assets/ssd_mobilenet.txt",
         numThreads: 1, // defaults to 1
         isAsset: true, // defaults to true, set to false to load resources outside assets
         useGpuDelegate: false // defaults to false, set to true to use GPU delegate
@@ -46,10 +63,10 @@ class ScanController extends GetxController{
       if (e is CameraException) {
         switch (e.code) {
           case 'CameraAccessDenied':
-          print('Ati refuzat accesul aplicatiei la camera');
+          log('Ati refuzat accesul aplicatiei la camera');
             break;
           default:
-          print('Erori neclasificate');
+          log('Erori neclasificate');
             break;
         }
       }
@@ -57,26 +74,34 @@ class ScanController extends GetxController{
   }
 
   Future<void> objectRecognition(CameraImage img) async{
-    var recognitions = await Tflite.runModelOnFrame(
+    var recognitions = await Tflite.detectObjectOnFrame(
         bytesList: img.planes.map((plane) {return plane.bytes;}).toList(),// required
+        model: "SSDMobileNet",
         imageHeight: img.height,
         imageWidth: img.width,
         imageMean: 127.5,   // defaults to 127.5
         imageStd: 127.5,    // defaults to 127.5
         rotation: 90,       // defaults to 90, Android only
-        numResults: 2,      // defaults to 5
+        numResultsPerClass: 2, // defaults to 5
         threshold: 0.1,     // defaults to 0.1
         asynch: true        // defaults to true
     );
 
-    print(recognitions);
+    if (recognitions != null) {
+      var detectedObj = recognitions.first;
+      log("result is $detectedObj");
+      if (detectedObj['confidenceInClass'] * 100 > 45) {
+        label = detectedObj['detectedClass'].toString();
+        h = detectedObj['rect']['h'];
+        w = detectedObj['rect']['w'];
+        x = detectedObj['rect']['x'];
+        y = detectedObj['rect']['y'];
+      }
+      update();
+      _isInitialized.refresh();
+    }
   }
 
-  @override
-  void onInit() {
-    initCamera();
-    _initTflite();
-    super.onInit();
-  }
+
 
 }
